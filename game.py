a3c94e283f22dc6298c135a316954f89c4b39b2b
Ana Margarida <anamargaridarl@gmail.com>
2020-03-14 22:27:48 +0000

    added game display function

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+
 GameBoard = [[0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0],
              [0,0,0,0,-1,-1,0,0],
@@ -7,9 +8,29 @@ GameBoard = [[0,0,0,0,0,0,0,0],
              [0,0,-2,0,0,0,0,0],
              [0,0,0,0,0,0,0,0]]
 
-
 GoalSquares = [[2,6]]
 
-def game():
 
-main()
+def display_game():
+    
+    print('\n')
+    print('                 Zhed Game    \n')
+
+    Coordenates = [0,0,1,2,3,4,5,6,'']
+
+    row_format ="{:>4}" * (len(Coordenates) + 1)
+    print(row_format.format("y|x", *Coordenates))
+    print('   ------------------------------------')
+
+    Coords = []
+    for x in Coordenates:
+        Coords.append(str(x) +' |')
+    
+    for value, row in zip(Coords, GameBoard):
+        row2 = row + list('|')
+        print(row_format.format(value , *row2))
+        print('   ------------------------------------')
+        
+
+
+display_game()
